d7bf929fd2ea9c0da3abcfccc003da52424ce949
Murat Atay <murat.atay@smartiks.com.tr>
2018-12-03 21:52:50 +0300

    Smartiks.Framework.Net packages.

---
+++ src/Smartiks.Framework.Net.Abstractions/HttpClientOptions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Smartiks.Framework.Net.Abstractions
+{
+    public class HttpClientOptions
+    {
+        public TimeSpan TimeOut { get; set; }
+    }
+}
---
+++ src/Smartiks.Framework.Net/HttpClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using Microsoft.Extensions.Options;
+using Smartiks.Framework.Net.Abstractions;
+
+namespace Smartiks.Framework.Net
+{
+    public class HttpClient<TOptions> : HttpClient
+        where TOptions : HttpClientOptions, new()
+    {
+        public TOptions Options { get; }
+
+        public HttpClient(IOptions<TOptions> optionsProvider)
+        {
+            Options = optionsProvider.Value;
+
+            Timeout = Options.TimeOut;
+        }
+    }
+}
