27a7bed88d53632e7534b738d23018acbf4e2dc9
Murat Atay <murat.atay@smartiks.com.tr>
2018-12-03 21:19:49 +0300

    Smartiks.Framework.Hosting packages.

---
+++ src/Smartiks.Framework.Hosting.Web.Abstractions/ServerHeaderOptions.cs
@@ -0,0 +1,7 @@
+namespace Smartiks.Framework.Hosting.Web.Abstractions
+{
+    public class ServerHeaderOptions
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Smartiks.Framework.Hosting.Web/ServerHeaderMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Smartiks.Framework.Hosting.Web.Abstractions;
+
+namespace Smartiks.Framework.Hosting.Web
+{
+    public class ServerHeaderMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        private readonly ServerHeaderOptions options;
+
+        public ServerHeaderMiddleware(RequestDelegate next, IOptions<ServerHeaderOptions> optionsProvider)
+        {
+            this.next = next;
+
+            options = optionsProvider.Value;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var response = context.Response;
+
+            response.OnStarting(() => {
+
+                response.Headers.Add("Server", options.Name);
+
+                return Task.CompletedTask;
+            });
+
+            await next.Invoke(context);
+        }
+    }
+}
