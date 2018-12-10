0ee47f7ccde2afb42dd362bfe423439c25d691a8
Murat Atay <murat.atay@smartiks.com.tr>
2018-12-10 15:30:16 +0300

    Smartiks.Framework.IO.Excel.UnitTests project added.

---
+++ test/Smartiks.Framework.IO.Excel.UnitTests/ExcelDocumentServiceTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Smartiks.Framework.IO.Excel.UnitTests.Models;
+using Smartiks.Framework.IO.Excel.Extensions;
+using Xunit;
+
+namespace Smartiks.Framework.IO.Excel.UnitTests
+{
+    public class ExcelDocumentServiceTest
+    {
+        private readonly ExcelDocumentService excelDocumentService;
+
+        public ExcelDocumentServiceTest()
+        {
+            excelDocumentService = new ExcelDocumentService();
+        }
+        
+
+        public static IEnumerable<object[]> ReadFromFileData()
+        {
+            return new List<object[]> {
+                new object[] {
+                    "Data/excel-model.xlsx",
+                    "Worksheet",
+                    CultureInfo.CurrentCulture,
+                    new [] {
+                        new ExcelModel {
+                            IntValue = 1,
+                            NullableIntValue = 1,
+                            DecimalValue = 1.5M,
+                            NullableDecimalValue = 1.5M,
+                            DateTimeValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            NullableDateTimeValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            DateValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            NullableDateValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            StringValue = "Text"
+                        }
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> WriteToFileData()
+        {
+            return new List<object[]> {
+                new object[] {
+                    "Data/excel-model-test.xlsx",
+                    "Worksheet",
+                    new [] {
+                        new ExcelModel {
+                            IntValue = 1,
+                            NullableIntValue = 1,
+                            DecimalValue = 1.5M,
+                            NullableDecimalValue = 1.5M,
+                            DateTimeValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            NullableDateTimeValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            DateValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            NullableDateValue = new DateTime(2018, 2, 1, 23, 10, 10),
+                            StringValue = "Text"
+                        }
+                    },
+                    CultureInfo.InvariantCulture
+                }
+            };
+        }
+
+
+        [Theory]
+        [MemberData(nameof(ReadFromFileData))]
+        public async Task ReadFromFile(string filePath, string worksheetName, CultureInfo cultureInfo, ExcelModel[] expectedExcelModels)
+        {
+            var excelModels = await excelDocumentService.ReadAsync<ExcelModel>(filePath, worksheetName, cultureInfo);
+
+            Assert.Equal(expectedExcelModels, excelModels);
+        }
+
+        [Theory]
+        [MemberData(nameof(WriteToFileData))]
+        public async Task WriteToFile(string filePath, string worksheetName, ExcelModel[] excelModels, CultureInfo cultureInfo)
+        {
+            await excelDocumentService.WriteAsync(filePath, worksheetName, excelModels, cultureInfo);
+        }
+    }
+}
---
+++ test/Smartiks.Framework.IO.Excel.UnitTests/Models/ExcelModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Smartiks.Framework.IO.Excel.UnitTests.Models
+{
+    public class ExcelModel
+    {
+        [Display(Name = "IntValueDisplayName")]
+        public int IntValue { get; set; }
+
+        public int? NullableIntValue { get; set; }
+
+        public decimal DecimalValue { get; set; }
+
+        [DisplayFormat(DataFormatString = "#,##0.00")]
+        public decimal? NullableDecimalValue { get; set; }
+
+        [DisplayFormat(DataFormatString = "m/d/yyyy")]
+        public DateTime DateTimeValue { get; set; }
+
+        [DisplayFormat(DataFormatString = "d/m/yyyy")]
+        public DateTime? NullableDateTimeValue { get; set; }
+
+        [DisplayFormat(DataFormatString = "yyyy-m-d")]
+        public DateTime DateValue { get; set; }
+
+        [DisplayFormat(DataFormatString = "[$-en-US]mmmm d, yyyy;@")]
+        public DateTime? NullableDateValue { get; set; }
+
+        public string StringValue { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            var model = obj as ExcelModel;
+            return model != null &&
+                   IntValue == model.IntValue &&
+                   EqualityComparer<int?>.Default.Equals(NullableIntValue, model.NullableIntValue) &&
+                   DecimalValue == model.DecimalValue &&
+                   EqualityComparer<decimal?>.Default.Equals(NullableDecimalValue, model.NullableDecimalValue) &&
+                   DateTimeValue == model.DateTimeValue &&
+                   EqualityComparer<DateTime?>.Default.Equals(NullableDateTimeValue, model.NullableDateTimeValue) &&
+                   DateValue == model.DateValue &&
+                   EqualityComparer<DateTime?>.Default.Equals(NullableDateValue, model.NullableDateValue) &&
+                   StringValue == model.StringValue;
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+            hash.Add(IntValue);
+            hash.Add(NullableIntValue);
+            hash.Add(DecimalValue);
+            hash.Add(NullableDecimalValue);
+            hash.Add(DateTimeValue);
+            hash.Add(NullableDateTimeValue);
+            hash.Add(DateValue);
+            hash.Add(NullableDateValue);
+            hash.Add(StringValue);
+            return hash.ToHashCode();
+        }
+    }
+}
