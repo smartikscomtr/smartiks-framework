dea21965c2f15bf87f3679981dc3fad4d098e6fd
Murat Atay <murat.atay@smartiks.com.tr>
2018-12-14 11:31:15 +0300

    Smartiks.Framework.Identity.Authentication.Owin packages.

---
+++ src/Smartiks.Framework.Identity.Authentication.Owin.Abstractions/IdentityAuthenticationClientOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Smartiks.Framework.Identity.Authentication.Owin.Abstractions
+{
+    public class IdentityAuthenticationClientOptions
+    {
+        /// <summary>
+        /// Base-address of the token issuer
+        /// </summary>
+        public string Authority { get; set; }
+    }
+}
---
+++ src/Smartiks.Framework.Identity.Authentication.Owin.Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IdentityServer3.AccessTokenValidation;
+using Owin;
+using Smartiks.Framework.Identity.Authentication.Owin.Abstractions;
+
+namespace Smartiks.Framework.Identity.Authentication.Owin.Extensions
+{
+    public static class AppBuilderExtensions
+    {
+        public static void UseIdentityAuthentication(this IAppBuilder app, Action<IdentityAuthenticationClientOptions> optionsAction)
+        {
+            var options = new IdentityAuthenticationClientOptions();
+
+            optionsAction?.Invoke(options);
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions {
+                Authority = options.Authority,
+                DelayLoadMetadata = true
+            });
+        }
+    }
+}
