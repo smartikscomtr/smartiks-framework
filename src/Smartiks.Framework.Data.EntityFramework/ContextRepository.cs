404306254a372994d723745018a54eae9c7b5b31
Murat Atay <murat.atay@smartiks.com.tr>
2019-01-04 11:11:15 +0300

    Smartiks.Framework.Data.EntityFramework

---
+++ src/Smartiks.Framework.Data.EntityFramework/ContextRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Smartiks.Framework.Data.Abstractions;
+
+namespace Smartiks.Framework.Data.EntityFramework
+{
+    public class ContextRepository<TContext, TQueryable, TListQuery, TId> : ReadOnlyContextRepository<TContext, TQueryable, TListQuery, TId>, IQueryableService<TQueryable, TListQuery, TId>
+        where TContext : DbContext
+        where TQueryable : class, IId<TId>
+        where TListQuery : Query<TQueryable>
+        where TId : struct
+    {
+        protected DbSet<TQueryable> Set { get; }
+
+        public ContextRepository(TContext context) : base(context)
+        {
+            Set = context.Set<TQueryable>();
+        }
+
+        protected override IQueryable<TQueryable> GetQueryable()
+        {
+            return Set.AsNoTracking();
+        }
+
+        public virtual async Task<TId> InsertAsync<TModel>(TModel model, CancellationToken cancellationToken)
+            where TModel : class
+        {
+            var entity = Mapper.Map<TQueryable>(model);
+
+            Set.Add(entity);
+
+            await Context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+
+        public virtual async Task UpdateAsync<TModel>(TModel model, CancellationToken cancellationToken)
+            where TModel : class, IId<TId>
+        {
+            var entity = await Set.FirstAsync(e => e.Id.Equals(model.Id), cancellationToken);
+
+            Mapper.Map(model, entity);
+
+            await Context.SaveChangesAsync(cancellationToken);
+        }
+
+        public virtual async Task DeleteAsync(TId id, CancellationToken cancellationToken)
+        {
+            var entity = await Set.FirstAsync(e => e.Id.Equals(id), cancellationToken);
+
+            Context.Remove(entity);
+
+            await Context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ src/Smartiks.Framework.Data.EntityFramework/ReadOnlyContextRepository.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Smartiks.Framework.Data.Abstractions;
+using Smartiks.Framework.Data.Extensions;
+
+namespace Smartiks.Framework.Data.EntityFramework
+{
+    public abstract class ReadOnlyContextRepository<TContext, TQueryable, TListQuery, TId> : IReadOnlyQueryableService<TQueryable, TListQuery, TId>
+        where TContext : DbContext
+        where TQueryable : class, IId<TId>
+        where TListQuery : Query<TQueryable>
+        where TId : struct
+    {
+        protected TContext Context { get; }
+
+        protected ReadOnlyContextRepository(TContext context)
+        {
+            Context = context;
+        }
+
+        protected abstract IQueryable<TQueryable> GetQueryable();
+
+        public virtual async Task<TModel> GetAsync<TModel>(TId id, CancellationToken cancellationToken)
+            where TModel : class
+        {
+            var queryable =
+                GetQueryable()
+                    .Where(q => q.Id.Equals(id));
+
+            return await queryable.ProjectTo<TModel>().FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public virtual async Task<QueryResult<TModel>> GetAsync<TModel>(TListQuery query, CancellationToken cancellationToken)
+            where TModel : class
+        {
+            var queryResult = new QueryResult<TModel>();
+
+
+            var queryable = GetQueryable();
+
+            if (query != null)
+            {
+                if (query.Criteria != null && query.Criteria.Expression != null)
+                {
+                    queryable = queryable.Where(query.Criteria.Expression);
+                }
+
+                if (query.Orders != null)
+                {
+                    foreach (var queryOrder in query.Orders)
+                    {
+                        queryable = queryOrder.ApplyTo(queryable);
+                    }
+                }
+
+                if (query.Segment != null)
+                {
+                    queryResult.TotalCount = await queryable.CountAsync(cancellationToken);
+
+                    queryable =
+                        queryable
+                            .Skip(query.Segment.StartIndex)
+                            .Take(query.Segment.Count ?? int.MaxValue);
+                }
+            }
+
+            queryResult.Items = await queryable.ProjectTo<TModel>().ToListAsync(cancellationToken);
+
+            return queryResult;
+        }
+    }
+}
