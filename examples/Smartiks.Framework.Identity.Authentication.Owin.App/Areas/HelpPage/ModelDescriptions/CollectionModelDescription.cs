9f27b857b6b275e86992b428ce99e75dfce586ad
mehyaa <mehyaa@gmail.com>
2019-06-20 15:20:57 +0300

    OWIN projects removed

--- examples/Smartiks.Framework.Identity.Authentication.Owin.App/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Smartiks.Framework.Identity.Authentication.Owin.App.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
