2e800cf2536fd34f06507967013ff0ee048b26d0
mehyaa <mehyaa@gmail.com>
2019-10-30 14:35:37 +0300

    ITemplateService interface fixed

--- src/Smartiks.Framework.Text.Abstractions/ITemplateService.cs
+++ src/Smartiks.Framework.Text.Abstractions/ITemplateService.cs
@@ -1,6 +1,9 @@
-﻿namespace Smartiks.Framework.Text.Abstractions
+﻿using System;
+
+namespace Smartiks.Framework.Text.Abstractions
 {
     public interface ITemplateService
     {
+        string Format<TContext>(string template, TContext context, Func<string, string> transform, IFormatProvider formatProvider);
     }
 }
