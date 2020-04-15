1b300beaeecc4eb36efb7f172c532a378342cc51
Murat Atay <murat.atay@smartiks.com.tr>
2020-04-15 17:12:18 +0300

    Messagepack updated to latest version.

--- src/Smartiks.Framework.Caching.Extensions/DistributedCacheExtensions.cs
+++ src/Smartiks.Framework.Caching.Extensions/DistributedCacheExtensions.cs
@@ -8,6 +8,12 @@ namespace Smartiks.Framework.Caching.Extensions
 {
     public static class DistributedCacheExtensions
     {
+        public static MessagePackSerializerOptions Options { get; } =
+            ContractlessStandardResolver.Options
+                .WithOmitAssemblyVersion(true)
+                .WithAllowAssemblyVersionMismatch(true)
+                .WithCompression(MessagePackCompression.Lz4BlockArray);
+
         public static async Task<T> GetAsync<T>(this IDistributedCache distributedCache, string key, CancellationToken token = default)
         {
             var bytes = await distributedCache.GetAsync(key, token);
@@ -15,12 +21,12 @@ namespace Smartiks.Framework.Caching.Extensions
             if (bytes == null)
                 return default;
 
-            return LZ4MessagePackSerializer.Deserialize<T>(bytes, ContractlessStandardResolver.Instance);
+            return MessagePackSerializer.Deserialize<T>(bytes, Options);
         }
 
         public static async Task SetAsync<T>(this IDistributedCache distributedCache, string key, T value, DistributedCacheEntryOptions options, CancellationToken token = default)
         {
-            var bytes = LZ4MessagePackSerializer.Serialize(value, ContractlessStandardResolver.Instance);
+            var bytes = MessagePackSerializer.Serialize(value, Options);
 
             await distributedCache.SetAsync(key, bytes, options, token);
         }
